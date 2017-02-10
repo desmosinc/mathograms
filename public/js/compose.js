4740528fd54d624f26eced79355a819347dbaec9
Chris Lusto <ctlusto@gmail.com>
2017-02-10 15:49:40 -0500

    Remove email feature

--- public/js/compose.js
+++ public/js/compose.js
@@ -8,8 +8,6 @@
   var $twitterLink = $('.tweet-btn');
   var $facebookLink = $('.fb-post-btn');
   var $headerImages = $('.header .images');
-  var $recipientsField = $('.share-form .recipients');
-  var $nameField = $('.share-form .name');
   var $composeView = $('.compose-view');
   var $shareView = $('.share-view');
   var $shareLink = $('.share-link');
@@ -64,32 +62,6 @@
     r.execute(shareCallback);
   };
 
-  //**
-  // handle logic for sending an e-mail.
-  // posts to /send, and uses mandrill to actually send the email
-  //**
-  var sendEmail = function () {
-    $body.addClass('is-loading');
-    return $.post('/send', {
-      recipients: $recipientsField.val(),
-      name: $nameField.val(),
-      link: lastSavedURL
-    }).done(function () {
-      $body.removeClass('is-loading');
-      $('.success-message').show();
-      $recipientsField.val('');
-      setTimeout(function() {
-        $('.success-message').fadeOut();
-      }, 2000)
-    }).fail(function () {
-      $body.removeClass('is-loading');
-      $('.error-message').show();
-      setTimeout(function() {
-        $('.error-message').fadeOut();
-      }, 2000)
-    })
-  };
-
   //this is the code that interacts with the graphpaper
   var clickToSetGraph = function() {
     $('.button.selected').removeClass('selected');
@@ -117,7 +89,6 @@
   $('.action-create-another').on('click', showCompose)
   $shareLink.on('click', function() {this.select();});
   $('.button').on('click', clickToSetGraph);
-  $('.action-send').on('click', sendEmail);
 
   //set the state of the graph
   selectGraph('sierpinski');
